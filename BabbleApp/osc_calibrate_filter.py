3f4d23a78a2d77dfedabae17709d4571c79e92c8
Rames The Generic <53163624+RamesTheGeneric@users.noreply.github.com>
2023-09-14 19:51:16 -0400

    Replaced clip function

--- BabbleApp/osc_calibrate_filter.py
+++ BabbleApp/osc_calibrate_filter.py
@@ -76,9 +76,4 @@ class cal():
                 calibrated_array[i] = calibrated_value
             array = calibrated_array
 
-        for i, value in enumerate(array):   # Clamp output between 0-1
-            min_value = self.min_max_array[0, i]
-            calibrated_value = (value - 0) / (1.0 - 0)
-            array[i] = calibrated_value
-
-        return array
+        return np.clip(array,0,1)   # Clamp outputs between 0-1
